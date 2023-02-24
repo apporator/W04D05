449496a19688d63e169aed5df1b6ff20843b6768
bann-dito <ed.bacsierra@gmail.com>
2023-02-24 12:05:54 -0800

    Add linear my_min

--- execution_time.rb
+++ execution_time.rb
@@ -1,23 +1,37 @@
 
 #this is very bad, example of a quadratic classification
+# def my_min(arr)
+#     return nil if arr.empty?
+
+#     answer = arr[0]
+
+#     arr.each do |ele1|
+#         arr.each do |ele2|
+#             if ele2 > ele1
+#                 copmarison = ele1
+#                 if answer > copmarison
+#                     answer = copmarison
+#                 end
+#             end
+#         end
+#     end
+#     answer
+# end
+
+arr = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+
+# p my_min(arr)
+
 def my_min(arr)
     return nil if arr.empty?
 
-    answer = arr[0]
+    answer = arr.first
 
-    arr.each do |ele1|
-        arr.each do |ele2|
-            if ele2 > ele1
-                copmarison = ele1
-                if answer > copmarison
-                    answer = copmarison
-                end
-            end
-        end
+    arr.each do |ele|
+        answer = ele if answer > ele
     end
+    
     answer
 end
-
-arr = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 
 p my_min(arr)
